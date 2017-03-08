8f951ed906644e35570cbbca91b0429839619950
davidkorff <davidkorff@gmail.com>
2017-03-08 14:49:35 -0500

    format changes

--- index.js
+++ index.js
@@ -1,60 +1,48 @@
-letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 document.addEventListener('keydown', function(e) {
   if (e.which === 13) {
     i=0
-    document.getElementById("score").innerHTML = i
     secs = 3000
     startGame()
+    document.getElementById("score").innerHTML = i
     document.getElementById("time").innerHTML = secs
     myTimer()
   }
 })
 
-function startGame(){
-  let letter = letters[Math.floor((Math.random()*26))]
-  document.getElementById("game").innerHTML = letter.toUpperCase()
-  didYouGetIt()
+function myTimer(){
+  let id = setInterval(function(){
+    secs--
+    document.addEventListener('keydown', function(e) {
+      if (e.which === 13) {
+        clearInterval(id)}})
+        if(secs<=0){
+          secs = 0
+          document.getElementById("time").innerHTML = secs
+          document.getElementById("final").innerHTML = `Your Score is ${i}`
+          document.getElementById("score").innerHTML = i
+          document.removeEventListener("keypress", pressLetter) //--clearInterval(id)
+        }
+        document.getElementById("time").innerHTML = secs
+      }, 10)
 }
 
-function didYouGetIt(){
+function startGame(){
+  var letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  let letter = letters[Math.floor((Math.random()*26))]
+  document.getElementById("game").innerHTML = letter
   document.addEventListener("keypress", pressLetter)
 }
 
 function pressLetter(e){
   let letter = e.target.getElementsByTagName("p").game.innerHTML.toLowerCase()
   if (e.key === letter) {
-    addToScore()
+    document.getElementById("score").innerHTML=++i
     document.removeEventListener("keypress", pressLetter)
     startGame()
   }
   else {
-    subtractToScore()
+    if(i>0){
+      document.getElementById("score").innerHTML=--i
+    }
   }
 }
-
-function subtractToScore(){
-  if(i>0){
-    i--
-    document.getElementById("score").innerHTML=i
-  }
-}
-
-function addToScore(){
-  i++
-  document.getElementById("score").innerHTML=i
-}
-function myTimer(){
-  let id = setInterval(function(){
-      secs--
-  document.addEventListener('keydown', function(e) {
-      if (e.which === 13) {
-        clearInterval(id)}})
-    if(secs<=0){
-      secs = 0
-      document.getElementById("time").innerHTML = secs
-      document.getElementById("final").innerHTML = `Your Score is ${i}`
-      document.getElementById("score").innerHTML = i
-      document.removeEventListener("keypress", pressLetter) //--clearInterval(id)
-     }
-     document.getElementById("time").innerHTML = secs
-  }, 10)}
